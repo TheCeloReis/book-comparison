11bd706e1a98add6b353463af1bdd2449c64d05f
Celo Reis <hello@celoreis.dev>
2021-10-24 10:43:46 -0300

    chore: setup react query

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 import GlobalStyle from "../styles/GlobalStyle";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme} />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <GlobalStyle />
+        <ThemeProvider theme={theme} />
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
